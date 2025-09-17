4255df045d35a4fd533027adf8aff329e58373c5
Abhijeetk1000 <mydatascience88@gmail.com>
2025-09-17 17:38:49 +0530

    Multiplication code to new branch

--- P1.py
+++ P1.py
@@ -4,8 +4,11 @@ def add(x,y):
 def sub(x,y):
     return x-y
 
+def mul(x,y):
+    return x*y
 m = int(input("Num1: "))
 n = int(input("Num2: "))
 
 print("Add: ",add(m,n))
 print("Sub: ", sub(m,n))
+print("Mul: ", mul(m,n))
